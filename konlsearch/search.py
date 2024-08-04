4e4cb44321890af9866414beef390fbd49e5c238
Byoung Gi Lee <handrake@gmail.com>
2024-08-05 00:55:57 +0900

    style: cleanup

--- konlsearch/search.py
+++ konlsearch/search.py
@@ -7,10 +7,9 @@ class KonlSearch:
     def __init__(self, path):
         self.path = path
         self.options = rocksdict.Options()
-        options = rocksdict.Options()
-        options.create_if_missing(True)
-        options.create_missing_column_families(True)
-        self.db = rocksdict.Rdict(path=self.path, options=options)
+        self.options.create_if_missing(True)
+        self.options.create_missing_column_families(True)
+        self.db = rocksdict.Rdict(path=self.path, options=self.options)
 
     def index(self, name) -> KonlIndex:
         return KonlIndexFactory.create_or_get(self.db, name)
@@ -20,5 +19,3 @@ class KonlSearch:
 
     def destroy(self):
         rocksdict.Rdict.destroy(self.path)
-
-# class SearchOption(enum.Enum):
