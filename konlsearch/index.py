366c2567a81332ccec12c5c89b188ba231e3d27c
Byoung Gi Lee <handrake@gmail.com>
2024-08-10 11:25:09 +0900

    refactor: separate inverted_index

--- konlsearch/index.py
+++ konlsearch/index.py
@@ -1,20 +1,15 @@
 import re
 import typing
-import enum
 import threading
 
 import mecab
 import rocksdict
 
 from . import utility
+from .inverted_index import KonlInvertedIndex, TokenSearchMode
 from .lock import StripedLock
 
 mecab = mecab.MeCab()
-
-
-class TokenSearchMode(enum.StrEnum):
-    AND = enum.auto()
-    OR = enum.auto()
 
 
 SPECIAL_CHARACTERS = '@_!#$%^&*()<>?/\\|}{~:]",'
@@ -25,7 +20,7 @@ class KonlIndex:
         self._db = db
         self._name = name
         self._cf = utility.create_or_get_cf(db, name)
-        self._cf_inverted_index = utility.create_or_get_cf(db, self.build_inverted_index_name(name))
+        self._inverted_index = KonlInvertedIndex(db, name)
         self._locks = StripedLock(threading.Lock, 10)
 
     def index(self, document) -> int:
@@ -43,11 +38,7 @@ class KonlIndex:
             self._cf[last_document_id] = document
             self._cf[self.__build_token_name(last_document_id)] = list(tokens)
 
-            for token in tokens:
-                if token in self._cf_inverted_index:
-                    self._cf_inverted_index[token] |= {last_document_id}
-                else:
-                    self._cf_inverted_index[token] = {last_document_id}
+            self._inverted_index.index(last_document_id, tokens)
 
         return last_document_id
 
@@ -59,11 +50,7 @@ class KonlIndex:
             token_name = self.__build_token_name(document_id)
             tokens = self._cf[token_name]
 
-            for token in tokens:
-                self._cf_inverted_index[token] -= {document_id}
-
-                if not self._cf_inverted_index[token]:
-                    self._cf_inverted_index.delete(token)
+            self._inverted_index.delete(document_id, tokens)
 
             self._cf.delete(token_name)
             self._cf.delete(document_id)
@@ -75,40 +62,16 @@ class KonlIndex:
         return self._cf[self.__build_token_name(document_id)]
 
     # noinspection PyBroadException
-    def search(self, tokens: typing.List[int], mode: TokenSearchMode) -> typing.List[int]:
-        inverted_snapshot = self._cf_inverted_index.snapshot()
-
-        result_set = set()
-
-        for i, token in enumerate(tokens):
-            try:
-                document_ids = inverted_snapshot[token]
-            except Exception:
-                document_ids = set()
-
-            if mode == TokenSearchMode.OR:
-                result_set.update(document_ids)
-            elif mode == TokenSearchMode.AND:
-                if i == 0:
-                    result_set.update(document_ids)
-                else:
-                    result_set.intersection_update(document_ids)
-
-        del inverted_snapshot
-
-        return sorted(list(result_set))
+    def search(self, tokens: typing.List[str], mode: TokenSearchMode) -> typing.List[int]:
+        return self._inverted_index.search(tokens, mode)
 
     def close(self):
         self._cf.close()
-        self._cf_inverted_index.close()
+        self._inverted_index.close()
 
     @staticmethod
     def __build_token_name(document_id) -> str:
         return f'{document_id}:tokens'
-
-    @staticmethod
-    def build_inverted_index_name(index_name) -> str:
-        return f'{index_name}_inverted_index'
 
     @staticmethod
     def is_hangul(s) -> bool:
---
+++ konlsearch/inverted_index.py
@@ -0,0 +1,63 @@
+import rocksdict
+import typing
+import enum
+
+from . import utility
+
+
+class TokenSearchMode(enum.StrEnum):
+    AND = enum.auto()
+    OR = enum.auto()
+
+
+class KonlInvertedIndex:
+    def __init__(self, db: rocksdict.Rdict, name: str):
+        self._db = db
+        self._name = self.__build_inverted_index_name(name)
+        self._cf = utility.create_or_get_cf(db, self._name)
+
+    def close(self):
+        self._cf.close()
+
+    def index(self, document_id: int, tokens: typing.List[str]):
+        for token in tokens:
+            if token in self._cf:
+                self._cf[token] |= {document_id}
+            else:
+                self._cf[token] = {document_id}
+
+    def delete(self, document_id: int, tokens: typing.List[str]) -> None:
+        for token in tokens:
+            if token in self._cf:
+                self._cf[token] -= {document_id}
+
+            if not self._cf[token]:
+                self._cf.delete(token)
+
+    # noinspection PyBroadException
+    def search(self, tokens: typing.List[int], mode: TokenSearchMode) -> typing.List[int]:
+        snapshot = self._cf.snapshot()
+
+        result_set = set()
+
+        for i, token in enumerate(tokens):
+            try:
+                document_ids = snapshot[token]
+            except Exception:
+                document_ids = set()
+
+            if mode == TokenSearchMode.OR:
+                result_set.update(document_ids)
+            elif mode == TokenSearchMode.AND:
+                if i == 0:
+                    result_set.update(document_ids)
+                else:
+                    result_set.intersection_update(document_ids)
+
+        del snapshot
+
+        return sorted(list(result_set))
+
+    @staticmethod
+    def __build_inverted_index_name(name: str) -> str:
+        return f'{name}_inverted_index'
