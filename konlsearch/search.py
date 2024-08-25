f4c4c4f20c0946f0fba21b75c1622da36e9775d3
Byoung Gi Lee <handrake@gmail.com>
2024-08-25 21:41:53 +0900

    feat: add access_type

--- konlsearch/search.py
+++ konlsearch/search.py
@@ -1,17 +1,28 @@
+import enum
 import typing
 
 import rocksdict
+from strenum import StrEnum
 
 from .index import KonlIndex
 
 
+class AccessType(StrEnum):
+    READ_WRITE = enum.auto()
+    READ_ONLY = enum.auto()
+
+
 class KonlSearch:
-    def __init__(self, path):
+    def __init__(self, path: str, access_type: AccessType = AccessType.READ_WRITE):
         self.path = path
         self.options = rocksdict.Options()
         self.options.create_if_missing(True)
         self.options.create_missing_column_families(True)
-        self.db = rocksdict.Rdict(path=self.path, options=self.options)
+
+        access_type = rocksdict.AccessType.read_write() \
+            if (access_type == AccessType.READ_WRITE) else rocksdict.AccessType.read_only()
+
+        self.db = rocksdict.Rdict(path=self.path, options=self.options, access_type=access_type)
         self._index_prefix = "index"
 
     def index(self, name) -> KonlIndex:
